575da0ac2ec8cb3ecdca938adfc505dc61f8f468
geocode-matt <mlindenx@gmail.com>
2020-09-27 15:16:58 -0500

    tests complete

--- src/components/About/__tests__/index.test.js
+++ src/components/About/__tests__/index.test.js
@@ -3,18 +3,32 @@ import { render, cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import About from '..';
 
+const categories = [
+    { name: 'portraits', description: 'Portraits of people in my life' }
+  ]
+  const mockCurrentCategory = jest.fn();
+  const mockSetCurrentCategory = jest.fn();
+
 afterEach(cleanup);
 
 describe('About component', () => {
     // First Test
     it('renders', () => {
-        render(<About />);
-    })
+        render(<About
+          categories={categories}
+          setCurrentCategory={mockSetCurrentCategory}
+          currentCategory={mockCurrentCategory}
+        />);
+      })
   
     // Second Test
     it('matches snapshot DOM node structure', () => {
         // render About
-        const { asFragment } = render(<About />);
+        const { asFragment } = render(<About
+            categories={categories}
+            setCurrentCategory={mockSetCurrentCategory}
+            currentCategory={mockCurrentCategory}
+          />);
         expect(asFragment()).toMatchSnapshot();
     })
   })
